d5b798f484a6ed3bab0cfa0409a15f41e5739366
jordan-storz <jordan.storz.1@gmail.com>
2017-01-26 10:41:03 -0700

    static serves documentation

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'doc')));
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
 
 
 module.exports = router;
